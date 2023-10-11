1df681c8e3859f2ecad39355fee34df3e3efdf6d
macbook kal <arnaud.pantenier@gmail.com>
2023-10-11 15:40:08 +0200

    Ajout liste des adresses

---
+++ src/types/Adresses.tsx
@@ -0,0 +1,17 @@
+type Adresses = {
+    id: string,
+    jours: string,
+    heures: string,
+    adresse: string,
+    ville: string
+}
+
+export const ListAdresses: Adresses[] = [
+    {
+        id: "sevigne",
+        jours: "Du lundi au samedi",
+        heures: "9h à 19h",
+        adresse: "11 rue Madame de Sévigné",
+        ville: "26300 Bourg de Péage"
+    },
+]
