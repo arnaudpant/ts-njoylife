23523fd99e2ea3e9a46baa4bd48578ef1a06f4ee
macbook kal <arnaud.pantenier@gmail.com>
2023-10-19 15:35:21 +0200

    Modif padding btn

--- src/components/UI/buttons/Bouton.tsx
+++ src/components/UI/buttons/Bouton.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 const Bouton = ({href, label}: Props) => {
     return (
-        <button className="px-4 py-2 bg-action text-blanc rounded-full w-60">
+        <button className="px-4 py-3 bg-action text-blanc rounded-full w-60">
             <a href={href}>{label}</a>
         </button>
     );
