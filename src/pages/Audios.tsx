de68248fb2ac6b60760bd07d125d079b0e062bfc
Kal-PC-FIXE <arnaud.pantenier@gmail.com>
2023-10-21 17:36:55 +0200

    modif page audio

--- src/pages/Audios.tsx
+++ src/pages/Audios.tsx
@@ -1,58 +1,81 @@
 import Plumes from "../components/Plumes";
-import Sections from "../components/UI/sections/Sections";
 
 
 
 const Audios = () => {
     return (
-        <Sections fond="vert">
-            <h1 className="text-2xl md:text-4xl font-semibold m-5">RELAXATIONS GUIDEES</h1>
-            <div className="container mx-auto flex flex-col gap-4">
-                <div>
-                    <h3>Les points d'appui du corps</h3>
-                    <audio controls preload="auto" src="../audio/Relax1 points-appui.m4a"
-                        controlsList="nodownload">
-                        Votre navigateur ne supporte pas la balise audio
-                    </audio>
-                </div>
-                <h3>Respiration carrée sur 6 temps</h3>
+        <div className="flex flex-col items-center md:justify-evenly w-full bg-primaire min-h-[calc(100vh-112px)]">
 
-                <audio controls preload="auto" src="../audio/Relax2 respiration-carree.m4a"
-                    controlsList="nodownload">
-                    Votre navigateur ne supporte pas la balise audio
-                </audio>
-                <div>
-                    <h3>Libérez votre mental</h3>
-                    <audio controls preload="auto" src="../audio/Relax3 libérez votre mental.m4a"
-                        controlsList="nodownload">
-                        Votre navigateur ne supporte pas la balise audio
-                    </audio>
-                </div>
-                <div>
-                    <h3>Les vagues du souffle</h3>
-                    <audio controls preload="auto" src="../audio/Relax4 Les vagues du souffle.m4a"
-                        controlsList="nodownload">
-                        Votre navigateur ne supporte pas la balise audio
-                    </audio>
-                </div>
-                <div>
-                    <h3>L'envol</h3>
-                    <audio controls preload="auto" src="../audio/Relax5 envol.m4a" controlsList="nodownload">Votre
-                        navigateur ne supporte pas la balise audio
-                    </audio>
-                </div>
-                <div>
-                    <h3>Ouverture et réceptivité</h3>
-                    <audio controls preload="auto" src="../audio/Relax6 ouverture.m4a"
-                        controlsList="nodownload">
-                        Votre navigateur ne supporte pas la balise audio
-                    </audio>
-                </div>
+            <h1 className="text-2xl md:text-4xl font-semibold mx-5 mt-16 md:mt-4">RELAXATIONS GUIDEES</h1>
+
+            <div className="rounded-full max-w-[680px] shadow-card overflow-hidden m-8">
+                <img src="/photos/photo-audio.jpg" alt="femme ecoutant de la musique sur son lit avec un casque" />
             </div>
-            <div className="mt-8">
-                <Plumes />
+
+
+            <div className="container mx-auto h-full flex flex-col md:flex-row items-center md:justify-center gap-8 mb-8 px-4">
+
+                <div>
+                    <div className="px-4">
+                        <h3 className="pb-2">Les points d'appui du corps</h3>
+                        <audio controls preload="auto" src="/audio/Relax1 points-appui.m4a"
+                            controlsList="nodownload">
+                            Votre navigateur ne supporte pas la balise audio
+                        </audio>
+                    </div>
+
+
+                    <div className="px-4">
+                        <h3 className="pb-2">Respiration carrée sur 6 temps</h3>
+                        <audio controls preload="auto" src="/audio/Relax2 respiration-carree.m4a"
+                            controlsList="nodownload">
+                            Votre navigateur ne supporte pas la balise audio
+                        </audio>
+                    </div>
+
+
+                    <div className="px-4">
+                        <h3 className="pb-2">Libérez votre mental</h3>
+                        <audio controls preload="auto" src="/audio/Relax3 libérez votre mental.m4a"
+                            controlsList="nodownload">
+                            Votre navigateur ne supporte pas la balise audio
+                        </audio>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="px-4">
+                        <h3 className="pb-2">Les vagues du souffle</h3>
+                        <audio controls preload="auto" src="/audio/Relax4 Les vagues du souffle.m4a"
+                            controlsList="nodownload">
+                            Votre navigateur ne supporte pas la balise audio
+                        </audio>
+                    </div>
+
+
+                    <div className="px-4">
+                        <h3 className="pb-2">L'envol</h3>
+                        <audio controls preload="auto" src="/audio/Relax5 envol.m4a" controlsList="nodownload">Votre
+                            navigateur ne supporte pas la balise audio
+                        </audio>
+                    </div>
+
+
+                    <div className="px-4">
+                        <h3>Ouverture et réceptivité</h3>
+                        <audio controls preload="auto" src="/audio/Relax6 ouverture.m4a"
+                            controlsList="nodownload">
+                            Votre navigateur ne supporte pas la balise audio
+                        </audio>
+                    </div>
+                </div>
+
+
+
+
             </div>
-        </Sections>
+            <Plumes />
+        </div>
     );
 };
 
