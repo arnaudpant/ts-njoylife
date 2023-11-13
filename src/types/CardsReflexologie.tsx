747b1b6d94566f8137f843c54e28f5cb0b4caf20
macbook kal <arnaud.pantenier@gmail.com>
2023-11-13 20:28:16 +0100

    commit avant push

---
+++ src/types/CardsReflexologie.tsx
@@ -0,0 +1,20 @@
+import { CardMassageInfo } from "./CardsMassageList";
+
+
+export const CardsReflexologieList: CardMassageInfo[] = [
+    {
+        imgSrc: "/cards/massages/massage-dien-chan.png",
+        imgAlt: "",
+        title: "Dien' Chan",
+        time: "60min",
+        link: "/massage-dien-chan" 
+    },
+    {
+        imgSrc: "/cards/massages/massage-chi-nei-tsang.png",
+        imgAlt: "",
+        title: "Chi Nei Tsang",
+        time: "60min",
+        link: "/massage-dien-chan" 
+    },
+]
+
