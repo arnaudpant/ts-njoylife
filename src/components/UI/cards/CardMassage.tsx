f9e98462da8b44d3021080144303f105567ac5f7
macbook kal <arnaud.pantenier@gmail.com>
2023-11-25 12:06:00 +0100

    remplacement a par link

--- src/components/UI/cards/CardMassage.tsx
+++ src/components/UI/cards/CardMassage.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 type CardMassageReflexologieInfo = {
     imgSrc: string,
     imgAlt: string,
@@ -10,13 +12,13 @@ const CardMassage = (card: CardMassageReflexologieInfo) => {
     const { imgSrc, imgAlt, title, time, link } = card
     return (
         <div>
-            <a href={`${link}`}>
+            <Link to={`${link}`}>
                 <div className="relative flex flex-col justify-center w-[300px] h-[200px] shadow-card rounded-xl bg-tealA overflow-hidden cursor-pointer">
                     <img src={imgSrc} alt={imgAlt} className="absolute w-full " />
                     <p className="absolute bg-tealI text-tealA text-2xl text-center py-1 rounded-full z-10 min-w-[180px] bottom-4 left-1/2 -translate-x-1/2 shadow-lg">{title}</p>
                 </div>
 
-            </a>
+            </Link>
             <p className="text-xl text-center py-3">{time}</p>
 
         </div>
--- src/components/UI/cards/CardsPacks.tsx
+++ src/components/UI/cards/CardsPacks.tsx
@@ -1,16 +1,17 @@
+import { Link } from "react-router-dom";
 import { CardPacks } from "../../../types/CardsPacksList";
 
 const CardsPacks = (card: CardPacks) => {
     const { imgSrc, imgAlt, title, link } = card
     return (
         <div className="flex flex-col w-[300px] shadow-card rounded-xl bg-blanc">
-            <a href={`${link}`}>
+            <Link to={`${link}`}>
                 <div className="relative h-[110px] flex flex-col items-center justify-center">
                     <img src={imgSrc} alt={imgAlt} className="absolute w-full" />
                     <h3 className="text-xl text-center pl-10">PACK</h3>
                     <h3 className="text-2xl text-center pl-10">{title}</h3>
                 </div>
-            </a>
+            </Link>
         </div>
     );
 };
