fa95b722f00ea52170c6756d7e71b282cea1c006
macbook kal <arnaud.pantenier@gmail.com>
2023-12-16 17:25:31 +0100

    ajout btn retour haut de page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,8 +5,39 @@ import SectionEvenements from "./SectionEvenements";
 import SectionHome from "./SectionHome";
 import SectionInfos from "../../components/UI/sections/SectionInfos";
 import SectionMassages from "./SectionMassages";
+import { useEffect, useState } from "react";
+import { ChevronUpCircle } from 'lucide-react';
 
 const Home: React.FC = () => {
+    const [appBarStyle, setAppBarStyle] = useState(
+        false
+    )
+
+    const handleClick = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+
+    useEffect(() => {
+        const handleScroll = (event: any): void => {
+            const scrollHeight = event.currentTarget.scrollY
+
+            if (scrollHeight > 800) {
+                setAppBarStyle(
+                    true
+                )
+            } else {
+                setAppBarStyle(
+                    false
+                )
+            }
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
+
     return (
         <>
             <SectionHome />
@@ -16,6 +47,13 @@ const Home: React.FC = () => {
             <SectionEvenements />
             <SectionCallToAction />
             <SectionInfos map={true} />
+            {
+                appBarStyle && (
+                    <div className="fixed right-4 bottom-4 animate-bounce cursor-pointer" onClick={handleClick} >
+                        <ChevronUpCircle className="text-tealG w-10 h-10" />
+                    </div>
+                )
+            }
         </>
     );
 };
