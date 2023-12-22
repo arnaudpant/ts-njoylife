4651f3ee1ef87f41e8d1c43c410872662ff38db6
macbook kal <arnaud.pantenier@gmail.com>
2023-12-22 19:23:20 +0100

    modif sport avec chatGPT

--- src/pages/coaching/Sport.tsx
+++ src/pages/coaching/Sport.tsx
@@ -23,27 +23,48 @@ const Sport = () => {
 
                 <div className="my-4">
                     <h2 className="text-xl md:text-2xl font-semibold my-4">
-                        Adultes et séniors, Pré & post natal
+                    Accompagnement pour Adultes, Séniors et Périodes Pré/Post-Natales
                     </h2>
+
+                    <h3 className="text-lg md:text-xl pt-6 pb-2">
+                    Rétablir l'Équilibre et la Fluidité Corporelle
+                    </h3>
                     
                     <p className="pb-2">
-                        Retrouvez la fluidité et la tonicité de votre corps par
-                        des exercices adaptés à vos besoins physiques et toutes
-                        vos particularités de santé, par la conscience du corps
-                        et de ses capacités via la fonction essentielle à la vie
-                        : <b>la respiration</b>
+                    Retrouvez la fluidité et la tonicité de votre corps grâce à des exercices spécifiques, 
+                    ajustés en fonction de vos particularités physiques et de votre santé. 
+                    Ces pratiques visent à développer la conscience corporelle et ses capacités, 
+                    en mettant en lumière la <b>respiration</b> comme pilier central de la vie.
                     </p>
                 </div>
 
                 <div className="my-4">
 
-                    <h3 className="text-lg md:text-xl pt-6 pb-2">Mes outils</h3>
+                    <h3 className="text-lg md:text-xl pt-6 pb-2">Outils à Votre Disposition</h3>
                     <ul className="pl-2 md:pl-4">
-                        <li>▸ Gym Pilates</li>
-                        <li>▸ Yoga</li>
-                        <li>▸ Entrainement fonctionnel</li>
-                        <li>▸ Ecole du dos</li>
+                        <li className="font-bold">▸ Gymnastique Pilates</li>
+                        <p className="pl-4 pb-4">
+                        Une méthode douce et efficace pour renforcer et assouplir le corps en travaillant sur la stabilité et l'alignement postural.
+                        </p>
+
+                        <li className="font-bold">▸ Yoga</li>
+                        <p className="pl-4 pb-4">
+                        Une pratique millénaire permettant d'harmoniser le corps, l'esprit et la respiration pour cultiver la souplesse et la sérénité intérieure.
+                        </p>
+
+                        <li className="font-bold">▸ Entrainement fonctionnel</li>
+                        <p className="pl-4 pb-4">
+                        Des exercices fonctionnels pour développer la force, la mobilité et améliorer les gestes du quotidien.
+                        </p>
+                        <li className="font-bold">▸ Ecole du dos</li>
+                        <p className="pl-4 pb-4">
+                        Des exercices spécifiques visant à renforcer la musculature profonde du dos pour prévenir les douleurs et maintenir une colonne vertébrale saine.
+                        </p>
                     </ul>
+
+                    <p>
+                        Ces outils sont conçus pour répondre à vos besoins spécifiques, que vous soyez adulte, senior ou que vous traversiez une période pré/post-natale.
+                    </p>
 
                 </div>
 
